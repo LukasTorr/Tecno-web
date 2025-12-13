bb2620bed30b3b38079bb2cb55e2fdcc36278e5c
Kamir <Kamiralfaro@gmail.com>
2025-12-12 21:03:03 -0300

    chore: add compra-carros component

---
+++ src/app/Components/compra-carros/compra-carros.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compra-carros',
+  templateUrl: './compra-carros.component.html',
+  styleUrls: ['./compra-carros.component.css']
+})
+export class CompraCarrosComponent {
+
+}
